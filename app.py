982fbe4764c96fffd8977d905f739ff02654286d
Gianluca <gianlucattaneo00@gmail.com>
2023-11-06 10:31:33 +0100

    Aggiunta files :)

---
+++ app.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
+
+# Crea l'applicazione FastAPI
+app = FastAPI()
+
+# Configura la connessione al database
+engine = create_async_engine(
+    "postgresql://user:password@host:port/database",
+    echo=True,
+)
+
+# Crea il sessione
+Session = sessionmaker(engine, expire_on_commit=False)
+
+# Crea il modello di dati
+class Person(BaseModel):
+    id: int
+    name: str
+    age: int
+
+# Definisci i metodi CRUD
+
+@app.get("/persons")
+async def get_persons():
+    with Session() as session:
+        persons = session.query(Person).all()
+    return persons
+
+@app.get("/persons/{id}")
+async def get_person(id: int):
+    with Session() as session:
+        person = session.query(Person).get(id)
+    return person
+
+@app.post("/persons")
+async def create_person(person: Person):
+    with Session() as session:
+        session.add(person)
+        session.commit()
+    return person
+
+@app.put("/persons/{id}")
+async def update_person(id: int, person: Person):
+    with Session() as session:
+        person_in_db = session.query(Person).get(id)
+        person_in_db.update(person)
+        session.commit()
+    return person
+
+@app.delete("/persons/{id}")
+async def delete_person(id: int):
+    with Session() as session:
+        person = session.query(Person).get(id)
+        session.delete(person)
+        session.commit()
+    return {"message": "Person deleted"}
+
+# Avvia l'applicazione
+if __name__ == "__main__":
+    app.run(debug=True)
